4d83e742aaf0696375d3dca93d8740db8e2f6ffa
Devon Crebbin <devon@artvuu.group>
2024-02-20 22:42:23 +1100

    fixed day night cycle on mobile

--- src/components/Sky.tsx
+++ src/components/Sky.tsx
@@ -11,6 +11,9 @@ export default function Sun(props: any) {
     repeat: Infinity,
   };
 
+  const dayTimeAnimation = ["brightness(0.7)", "brightness(1)", "brightness(0.9)", "brightness(0.3)"];
+  const nightTimeAnimation = ["brightness(0.2) grayscale(0.3)", "brightness(0.25) grayscale(0.2)", "brightness(0.3) grayscale(0.5)", "brightness(0.7) grayscale(0.3)"];
+
   useEffect(() => {
     const interval = setInterval(() => {
       setTimer((prev) => {
@@ -25,5 +28,5 @@ export default function Sun(props: any) {
     return () => clearInterval(interval);
   }, []);
 
-  return <motion.div className={"w-full h-full fixed z-[99] pointer-events-auto transition-all"} animate={{ backdropFilter: isDayTime ? ["brightness(0.7)", "brightness(1)", "brightness(0.9)", "brightness(0.3)"] : ["brightness(0.2) grayscale(0.3)", "brightness(0.25) grayscale(0.2)", "brightness(0.3) grayscale(0.5)", "brightness(0.7) grayscale(0.3)"] }} transition={animationTransition}></motion.div>;
+  return <motion.div className={"w-full h-full fixed z-[99] pointer-events-auto transition-all"} animate={{ WebkitBackdropFilter: isDayTime ? [...dayTimeAnimation] : nightTimeAnimation, backdropFilter: isDayTime ? dayTimeAnimation : [nightTimeAnimation] }} transition={animationTransition}></motion.div>;
 }

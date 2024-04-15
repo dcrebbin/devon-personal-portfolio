c8dda24844d5de9acf9d7accc179c78cdccb1cb9
Devon Crebbin <devon@artvuu.group>
2024-04-16 00:02:01 +1000

    i18n fix 2 via github isssue

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,10 +1,28 @@
+import en from "./public/locales/en/translation.json" assert { type: "json" };
+import hi from "./public/locales/hi/translation.json" assert { type: "json" };
+import zh from "./public/locales/zh/translation.json" assert { type: "json" };
+
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
   defaultLocale: "en",
   locales: ["en", "hi", "zh"],
   i18nextServer: {
-    backend: {
-      loadPath: "./public/locales/{{lng}}/{{ns}}.json",
+    resources: {
+      en: {
+        translation: {
+          ...en,
+        },
+      },
+      hi: {
+        translation: {
+          ...hi,
+        },
+      },
+      zh: {
+        translation: {
+          ...zh,
+        },
+      },
     },
   },
 };
